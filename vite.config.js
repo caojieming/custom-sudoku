fe11f8e4c4fd7b39759464380f558a0c5d7e12d0
James Cao <jcao5360@gmail.com>
2026-05-27 16:23:17 -0400

    build fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // change or set to '/' for custom domain or user/org page
+  base: 'custom-sudoku', // project name in Github
   plugins: [react()],
 })
